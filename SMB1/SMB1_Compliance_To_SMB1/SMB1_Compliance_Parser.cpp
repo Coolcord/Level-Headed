212315377f8fc2c9e7e15d33e2d518a3ebe05e9a
Coolcord <coolcord24@gmail.com>
2014-09-19 12:58:54 -0700

    Added some code to the parser

--- SMB1/SMB1_Compliance_To_SMB1/SMB1_Compliance_Parser.cpp
+++ SMB1/SMB1_Compliance_To_SMB1/SMB1_Compliance_Parser.cpp
@@ -1,5 +1,16 @@
 #include "SMB1_Compliance_Parser.h"
+#include "../Common SMB1 Files/Object_Item.h"
+#include "../Common SMB1 Files/Object_Item_String.h"
+#include "../Common SMB1 Files/Enemy_Item.h"
+#include "../Common SMB1 Files/Enemy_Item_String.h"
 
-SMB1_Compliance_Parser::SMB1_Compliance_Parser()
-{
+SMB1_Compliance_Parser::SMB1_Compliance_Parser() {
+
 }
+
+SMB1_Compliance_Parser::~SMB1_Compliance_Parser() {
+    delete this->objects;
+    delete this->enemies;
+}
+
+
--- SMB1/SMB1_Compliance_To_SMB1/SMB1_Compliance_Parser.h
+++ SMB1/SMB1_Compliance_To_SMB1/SMB1_Compliance_Parser.h
@@ -1,10 +1,20 @@
 #ifndef SMB1_COMPLIANCE_PARSER_H
 #define SMB1_COMPLIANCE_PARSER_H
 
+#include "../Common SMB1 Files/Object_Item.h"
+#include "../Common SMB1 Files/Enemy_Item.h"
+#include <QMap>
+#include <QString>
+
 class SMB1_Compliance_Parser
 {
 public:
     SMB1_Compliance_Parser();
+    ~SMB1_Compliance_Parser();
+
+private:
+    QMap<QString, Object_Item::Object_Item> *objects;
+    QMap<QString, Enemy_Item::Enemy_Item> *enemies;
 };
 
 #endif // SMB1_COMPLIANCE_PARSER_H
