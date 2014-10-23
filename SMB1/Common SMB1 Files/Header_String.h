78bfa1a89f88db4a9b28548efa14bb6e036e367b
Coolcord <coolcord24@gmail.com>
2014-10-22 20:00:48 -0700

    Added some more code to parse the header

---
+++ SMB1/Common SMB1 Files/Header_String.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_STRING_H
+#define HEADER_STRING_H
+
+#include "Level_Type_String.h"
+#include <QString>
+
+namespace Header {
+    const static QString STRING_NAME = Level_Type::STRING_NAME + "_Level";
+    const static QString STRING_TYPE = "Type";
+    const static QString STRING_ATTRIBUTE = "Attribute";
+    const static QString STRING_COMPLIMENT = "Compliment";
+    const static QString STRING_TIME = "Time";
+    const static QString STRING_MIDPOINT = "Midpoint";
+    const static QString STRING_LEVEL_LENGTH = "Level_Length";
+    const static QString STRING_NUMBER_OF_OBJECTS = "Number_Of_Objects";
+    const static QString STRING_NUMBER_OF_ENEMIES = "Number_Of_Enemies";
+    const static QString STRING_NUMBER_OF_PIPE_POINTERS = "Number_Of_Pipe_Pointers";
+}
+
+#endif // HEADER_STRING_H
