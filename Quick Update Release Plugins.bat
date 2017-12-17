5401f8c5a971b3bc99f7ceab28d865dad9d46142
Coolcord <coolcord24@tutanota.com>
2017-12-17 01:55:45 -0800

    Updated some build scripts

--- Quick Update Release Plugins.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-COLOR 0A
-ERASE E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Generators\SMB1_Compliance_Generator.dll
-ERASE E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
-ERASE E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Writers\SMB1_Writer.dll
-COPY E:\Level-Headed\SMB1\build-SMB1_Compliance_Generator-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\SMB1_Compliance_Generator.dll E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Generators\SMB1_Compliance_Generator.dll
-COPY E:\Level-Headed\SMB1\build-SMB1_Compliance_To_SMB1-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\SMB1_Compliance_To_SMB1.dll E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
-COPY E:\Level-Headed\SMB1\build-SMB1_Writer-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\SMB1_Writer.dll E:\Level-Headed\build-Level-Headed-Desktop_Qt_5_4_0_MinGW_32bit-Release\release\Plugins\Writers\SMB1_Writer.dll
-EXIT
