160cda25bce32e12137cdabb673c66e356640b79
Coolcord <coolcord24@tutanota.com>
2017-12-18 15:17:44 -0800

    Updated Linux build script with absolute paths for now

--- update_plugins.sh
+++ update_plugins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir -pv ./build-Level-Headed-Desktop-Debug/Plugins/Generators/
-mkdir -pv ./build-Level-Headed-Desktop-Debug/Plugins/Interpreters/
-mkdir -pv ./build-Level-Headed-Desktop-Debug/Plugins/Writers/
-cp -rvf ./SMB1/build-SMB1_Compliance_Generator-Desktop-Debug/libSMB1_Compliance_Generator.so ./build-Level-Headed-Desktop-Debug/Plugins/Generators/SMB1_Compliance_Generator.so
-cp -rvf ./SMB1/build-SMB1_Compliance_To_SMB1-Desktop-Debug/libSMB1_Compliance_To_SMB1.so ./build-Level-Headed-Desktop-Debug/Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
-cp -rvf ./SMB1/build-SMB1_Writer-Desktop-Debug/libSMB1_Writer.so ./build-Level-Headed-Desktop-Debug/Plugins/Writers/SMB1_Writer.so
+mkdir -pv /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Generators/
+mkdir -pv /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Interpreters/
+mkdir -pv /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Writers/
+cp -rvf /home/cord/Documents/Source_Code/Level-Headed/SMB1/build-SMB1_Compliance_Generator-Desktop-Debug/libSMB1_Compliance_Generator.so /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Generators/SMB1_Compliance_Generator.so
+cp -rvf /home/cord/Documents/Source_Code/Level-Headed/SMB1/build-SMB1_Compliance_To_SMB1-Desktop-Debug/libSMB1_Compliance_To_SMB1.so /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
+cp -rvf /home/cord/Documents/Source_Code/Level-Headed/SMB1/build-SMB1_Writer-Desktop-Debug/libSMB1_Writer.so /home/cord/Documents/Source_Code/Level-Headed/build-Level-Headed-Desktop-Debug/Plugins/Writers/SMB1_Writer.so
 echo "Plugins Updated!"
 exit 0
 
