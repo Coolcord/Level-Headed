90c5e90b78da8f06870795614947f38e77a9588a
Coolcord <coolcord24@tutanota.com>
2018-05-11 00:12:41 -0700

    Added a new class to manage the continuous enemies spawners

---
+++ SMB1/SMB1_Compliance_Generator/Continuous_Enemies_Spawner.cpp
@@ -0,0 +1,106 @@
+#include "Continuous_Enemies_Spawner.h"
+#include "../../Common_Files/Random.h"
+#include "Object_Writer.h"
+#include "Required_Enemy_Spawns.h"
+#include <array>
+#include <algorithm>
+#include <assert.h>
+
+Continuous_Enemies_Spawner::Continuous_Enemies_Spawner(SMB1_Compliance_Generator_Arguments *args, Object_Writer *object, Required_Enemy_Spawns *requiredEnemySpawns) {
+    assert(args);
+    assert(object);
+    assert(requiredEnemySpawns);
+    this->args = args;
+    this->object = object;
+    this->requiredEnemySpawns = requiredEnemySpawns;
+}
+
+bool Continuous_Enemies_Spawner::Create_Continuous_Enemies_Spawner(int x) {
+    //Pull these Values Based on the Level Type
+    int flyingCheepCheeps = 0;
+    int lakitus = 0;
+    int offscreenBulletBills = 0;
+    switch (this->args->levelType) {
+    default: assert(false);
+    case Level_Type::STANDARD_OVERWORLD:
+        flyingCheepCheeps = this->args->difficultyStandardOverworldFlyingCheepCheeps;
+        lakitus = this->args->difficultyStandardOverworldLakitus;
+        offscreenBulletBills = this->args->difficultyStandardOverworldOffscreenBulletBills;
+        break;
+    case Level_Type::UNDERGROUND:
+        flyingCheepCheeps = this->args->difficultyUndergroundFlyingCheepCheeps;
+        lakitus = this->args->difficultyUndergroundLakitus;
+        offscreenBulletBills = this->args->difficultyUndergroundOffscreenBulletBills;
+        break;
+    case Level_Type::UNDERWATER:
+        flyingCheepCheeps = this->args->difficultyUnderwaterFlyingCheepCheeps;
+        lakitus = this->args->difficultyUnderwaterLakitus;
+        offscreenBulletBills = this->args->difficultyUnderwaterSwimmingCheepCheeps;
+        break;
+    case Level_Type::BRIDGE:
+        flyingCheepCheeps = this->args->difficultyBridgeFlyingCheepCheeps;
+        lakitus = this->args->difficultyBridgeLakitus;
+        offscreenBulletBills = this->args->difficultyBridgeOffscreenBulletBills;
+        break;
+    case Level_Type::ISLAND:
+        flyingCheepCheeps = this->args->difficultyIslandFlyingCheepCheeps;
+        lakitus = this->args->difficultyIslandLakitus;
+        offscreenBulletBills = this->args->difficultyIslandOffscreenBulletBills;
+        break;
+    case Level_Type::CASTLE:
+        flyingCheepCheeps = this->args->difficultyCastleFlyingCheepCheeps;
+        lakitus = this->args->difficultyCastleLakitus;
+        offscreenBulletBills = this->args->difficultyCastleOffscreenBulletBills;
+        break;
+    }
+
+    //Try Lakitus First
+    if (this->args->difficulty >= lakitus && Random::Get_Num(99) <= this->args->difficultyLakituSpawnChancePerLevel-1) {
+        assert(this->requiredEnemySpawns->Add_Required_Enemy_Spawn(Enemy_Item::LAKITU, x, 0x2));
+        return true;
+    }
+
+    //Get the Specified Spawner by Priority
+    std::array<unsigned int, 3> order = {0,1,2};
+    if (this->args->difficultySpawnerPriority == 0) std::random_shuffle(order.begin(), order.end()); //random priority
+    for (unsigned int i = 0; i < order.size(); ++i) {
+        switch (order.at(i)) {
+        default: assert(false);
+        case 0:
+            if (this->Try_To_Create_Continuous_Lakitus(x, lakitus)) return true;
+            break;
+        case 1:
+            if (this->Try_To_Create_Continuous_Flying_Cheep_Cheeps(x, flyingCheepCheeps)) return true;
+            break;
+        case 2:
+            if (this->Try_To_Create_Continuous_Offscreen_Bullet_Bills(x, offscreenBulletBills)) return true;
+            break;
+        }
+    }
+    return false;
+}
+
+bool Continuous_Enemies_Spawner::Try_To_Create_Continuous_Flying_Cheep_Cheeps(int x, int expectedDifficulty) {
+    if (this->args->difficulty >= expectedDifficulty) {
+        if (this->object->Get_Absolute_X(x) == 0xF) assert(this->object->Flying_Cheep_Cheep_Spawner(x+1));
+        else assert(this->object->Flying_Cheep_Cheep_Spawner(x));
+    }
+    return false; //nothing was created
+}
+
+bool Continuous_Enemies_Spawner::Try_To_Create_Continuous_Lakitus(int x, int expectedDifficulty) {
+    if (this->args->difficulty >= expectedDifficulty && Random::Get_Num(99) <= this->args->difficultyLakituSpawnChancePerLevel-1) {
+        assert(this->requiredEnemySpawns->Add_Required_Enemy_Spawn(Enemy_Item::LAKITU, x, 0x2));
+        return true;
+    }
+    return false; //nothing was created
+}
+
+bool Continuous_Enemies_Spawner::Try_To_Create_Continuous_Offscreen_Bullet_Bills(int x, int expectedDifficulty) {
+    if (this->args->difficulty >= expectedDifficulty) {
+        if (this->object->Get_Absolute_X(x) == 0xF) assert(this->object->Bullet_Bill_Spawner(x+1));
+        else assert(this->object->Bullet_Bill_Spawner(x));
+        return true;
+    }
+    return false; //nothing was created
+}
