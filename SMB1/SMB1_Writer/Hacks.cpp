a87390b898bd1f838b484e398128a17d0fca0db1
Coolcord <coolcord24@tutanota.com>
2018-07-29 17:48:27 -0700

    Added the fixed bouncy spiny egg code

--- SMB1/SMB1_Writer/Hacks.cpp
+++ SMB1/SMB1_Writer/Hacks.cpp
@@ -42,8 +42,8 @@ bool Hacks::Black_Piranha_Plants() {
 }
 
 bool Hacks::Bouncy_Spiny_Eggs() {
-    if (!this->Write_Bytes_To_Offset(0x40FD, QByteArray::fromHex(QString("D0034CE2E0B416C012D0034CD9DF4CFADF").toLatin1()))) return false;
-    return this->Write_Bytes_To_Offset(0x6005, QByteArray::fromHex(QString("4CEDC0EAEA").toLatin1()));
+    if (!this->Write_Bytes_To_Offset(0x4101, QByteArray::fromHex(QString("D0034CE2E0B416C012D0034CD9DF20AEE1F0EF4CFFDF").toLatin1()))) return false;
+    return this->Write_Bytes_To_Offset(0x6005, QByteArray::fromHex(QString("4CF1C0").toLatin1()));
 }
 
 bool Hacks::Disable_Intro_Demo() {
