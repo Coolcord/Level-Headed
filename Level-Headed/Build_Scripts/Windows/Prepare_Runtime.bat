a57b09b0ecc58fce378e43b4909eb91c2783074f
Coolcord <coolcord24@tutanota.com>
2019-03-18 15:57:46 -0700

    Updated the Windows Prepare_Runtime.bat to use variables instead of hard coded paths

--- Level-Headed/Build_Scripts/Windows/Prepare_Runtime.bat
+++ Level-Headed/Build_Scripts/Windows/Prepare_Runtime.bat
@@ -1,17 +1,22 @@
 @ECHO OFF
 COLOR 0A
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Generators\SMB1_Compliance_Generator.dll
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Writers\SMB1_Writer.dll
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Hexagon.dll
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Sequential_Archive.dll
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Data\SMB1\Graphics.sa
-ERASE F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Data\SMB1\Music.sa
-COPY F:\Documents\Source_Code\Level-Headed\SMB1\build-SMB1_Compliance_Generator-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\SMB1_Compliance_Generator.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Generators\SMB1_Compliance_Generator.dll
-COPY F:\Documents\Source_Code\Level-Headed\SMB1\build-SMB1_Compliance_To_SMB1-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\SMB1_Compliance_To_SMB1.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
-COPY F:\Documents\Source_Code\Level-Headed\SMB1\build-SMB1_Writer-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\SMB1_Writer.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Writers\SMB1_Writer.dll
-COPY F:\Documents\Source_Code\Hexagon\build-Hexagon-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Hexagon.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Hexagon.dll
-COPY F:\Documents\Source_Code\Sequential_Archive\build-Sequential_Archive-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Sequential_Archive.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Plugins\Sequential_Archive.dll
-COPY F:\Documents\Source_Code\Level-Headed_Data\Graphics.sa F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Data\SMB1\Graphics.sa
-COPY F:\Documents\Source_Code\Level-Headed_Data\Music.sa F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_Qt_5_11_1_MinGW_32bit-Debug\debug\Data\SMB1\Music.sa
+
+SET QT=Qt_5_11_1_MinGW_32bit
+SET MODE=Debug
+SET SOURCE_LOCATION=F:\Documents\Source_Code
+
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Generators\SMB1_Compliance_Generator.dll
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Writers\SMB1_Writer.dll
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Hexagon.dll
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Sequential_Archive.dll
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Data\SMB1\Graphics.sa
+ERASE %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Data\SMB1\Music.sa
+COPY %SOURCE_LOCATION%\Level-Headed\SMB1\build-SMB1_Compliance_Generator-Desktop_%QT%-%MODE%\%MODE%\SMB1_Compliance_Generator.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Generators\SMB1_Compliance_Generator.dll
+COPY %SOURCE_LOCATION%\Level-Headed\SMB1\build-SMB1_Compliance_To_SMB1-Desktop_%QT%-%MODE%\%MODE%\SMB1_Compliance_To_SMB1.dll F:\Documents\Source_Code\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Interpreters\SMB1_Compliance_To_SMB1.dll
+COPY %SOURCE_LOCATION%\Level-Headed\SMB1\build-SMB1_Writer-Desktop_%QT%-%MODE%\%MODE%\SMB1_Writer.dll %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Writers\SMB1_Writer.dll
+COPY %SOURCE_LOCATION%\Hexagon\build-Hexagon-Desktop_%QT%-%MODE%\%MODE%\Hexagon.dll %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Hexagon.dll
+COPY %SOURCE_LOCATION%\Sequential_Archive\build-Sequential_Archive-Desktop_%QT%-%MODE%\%MODE%\Sequential_Archive.dll %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Plugins\Sequential_Archive.dll
+COPY %SOURCE_LOCATION%\Level-Headed_Data\Graphics.sa %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Data\SMB1\Graphics.sa
+COPY %SOURCE_LOCATION%\Level-Headed_Data\Music.sa %SOURCE_LOCATION%\Level-Headed\build-Level-Headed-Desktop_%QT%-%MODE%\%MODE%\Data\SMB1\Music.sa
 EXIT
