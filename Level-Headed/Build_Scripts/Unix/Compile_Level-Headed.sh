ee3a882d0e9a074db3dc00b315f72877c3d442f4
Coolcord <coolcord24@tutanota.com>
2019-03-20 00:35:13 -0700

    Updated the Unix build script to handle Unix on Windows

--- Level-Headed/Build_Scripts/Unix/Compile_Level-Headed.sh
+++ Level-Headed/Build_Scripts/Unix/Compile_Level-Headed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if qmake is installed
+# Check if dependencies are installed
 command -v qmake >/dev/null 2>&1 || { echo >&2 "qmake must be installed before Level-Headed can be compiled! Aborting!"; exit 1; }
 command -v git >/dev/null 2>&1 || { echo >&2 "git must be installed before Level-Headed can be compiled! Aborting!"; exit 1; }
 command -v nproc >/dev/null 2>&1 || { echo >&2 "nproc must be installed before Level-Headed can be compiled! Aborting!"; exit 1; }
@@ -80,21 +80,33 @@ mkdir -p Levels/SMB1/
 mkdir -p Plugins/Generators/
 mkdir -p Plugins/Interpreters/
 mkdir -p Plugins/Writers/
-
-# Install Plugins
-cp source/Level-Headed/Level-Headed/Level-Headed Level-Headed
-chmod +x Level-Headed
-cp source/Level-Headed/SMB1/SMB1_Compliance_Generator/libSMB1_Compliance_Generator.so Plugins/Generators/SMB1_Compliance_Generator.so
-cp source/Level-Headed/SMB1/SMB1_Compliance_To_SMB1/libSMB1_Compliance_To_SMB1.so Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
-cp source/Level-Headed/SMB1/SMB1_Writer/libSMB1_Writer.so Plugins/Writers/SMB1_Writer.so
-cp source/Hexagon/Hexagon/libHexagon.so Plugins/Hexagon.so
-cp source/Sequential_Archive/Sequential_Archive/libSequential_Archive.so Plugins/Sequential_Archive.so
-
-# Pack the Graphics and Music Packs
 mkdir -p source/Sequential_Archive/Sequential_Archive_Manager/Plugins/
-cp source/Sequential_Archive/Sequential_Archive/libSequential_Archive.so source/Sequential_Archive/Sequential_Archive_Manager/Plugins/Sequential_Archive.so
-source/Sequential_Archive/Sequential_Archive_Manager/Sequential_Archive_Manager --pack source/Level-Headed_Data/Graphics Data/SMB1/Graphics.sa
-source/Sequential_Archive/Sequential_Archive_Manager/Sequential_Archive_Manager --pack source/Level-Headed_Data/Music Data/SMB1/Music.sa
+
+# Install Plugins and Data
+releaseDirectory=source/Level-Headed/Level-Headed/release
+if [ -d "$releaseDirectory" ]; then # assume we're on a Unix environment running in a Windows OS
+    cp source/Level-Headed/Level-Headed/release/Level-Headed.exe Level-Headed.exe
+    chmod +x Level-Headed.exe
+    cp source/Level-Headed/SMB1/SMB1_Compliance_Generator/release/SMB1_Compliance_Generator.dll Plugins/Generators/SMB1_Compliance_Generator.dll
+    cp source/Level-Headed/SMB1/SMB1_Compliance_To_SMB1/release/SMB1_Compliance_To_SMB1.dll Plugins/Interpreters/SMB1_Compliance_To_SMB1.dll
+    cp source/Level-Headed/SMB1/SMB1_Writer/release/SMB1_Writer.dll Plugins/Writers/SMB1_Writer.dll
+    cp source/Hexagon/Hexagon/release/Hexagon.dll Plugins/Hexagon.dll
+    cp source/Sequential_Archive/Sequential_Archive/release/Sequential_Archive.dll Plugins/Sequential_Archive.dll
+    cp source/Sequential_Archive/Sequential_Archive/release/Sequential_Archive.dll source/Sequential_Archive/Sequential_Archive_Manager/Plugins/Sequential_Archive.dll
+    source/Sequential_Archive/Sequential_Archive_Manager/release/Sequential_Archive_Manager.exe --pack source/Level-Headed_Data/Graphics Data/SMB1/Graphics.sa
+    source/Sequential_Archive/Sequential_Archive_Manager/release/Sequential_Archive_Manager.exe --pack source/Level-Headed_Data/Music Data/SMB1/Music.sa
+else # assume we're on GNU/Linux or Mac
+    cp source/Level-Headed/Level-Headed/Level-Headed Level-Headed
+    chmod +x Level-Headed
+    cp source/Level-Headed/SMB1/SMB1_Compliance_Generator/libSMB1_Compliance_Generator.so Plugins/Generators/SMB1_Compliance_Generator.so
+    cp source/Level-Headed/SMB1/SMB1_Compliance_To_SMB1/libSMB1_Compliance_To_SMB1.so Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
+    cp source/Level-Headed/SMB1/SMB1_Writer/libSMB1_Writer.so Plugins/Writers/SMB1_Writer.so
+    cp source/Hexagon/Hexagon/libHexagon.so Plugins/Hexagon.so
+    cp source/Sequential_Archive/Sequential_Archive/libSequential_Archive.so Plugins/Sequential_Archive.so
+    cp source/Sequential_Archive/Sequential_Archive/libSequential_Archive.so source/Sequential_Archive/Sequential_Archive_Manager/Plugins/Sequential_Archive.so
+    source/Sequential_Archive/Sequential_Archive_Manager/Sequential_Archive_Manager --pack source/Level-Headed_Data/Graphics Data/SMB1/Graphics.sa
+    source/Sequential_Archive/Sequential_Archive_Manager/Sequential_Archive_Manager --pack source/Level-Headed_Data/Music Data/SMB1/Music.sa
+fi
 
 # Clean up
 rm -rf source/
