f24494710e79b81e8d5caaf4e5a94e03b2676053
Coolcord <coolcord24@tutanota.com>
2019-03-21 12:48:11 -0700

    Updated the Unix prepare script

--- Level-Headed/Build_Scripts/Unix/Prepare_Runtime.sh
+++ Level-Headed/Build_Scripts/Unix/Prepare_Runtime.sh
@@ -4,23 +4,23 @@ MODE="Debug"
 SOURCE_LOCATION="/home/cord/Documents/Source_Code"
 
 echo "Installing plugins for Level-Headed..."
-mkdir -p $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Data/SMB1/
-mkdir -p $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Generators/
-mkdir -p $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Interpreters/
-mkdir -p $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Writers/
-cp -rf $SOURCE_LOCATION/Level-Headed/SMB1/build-SMB1_Compliance_Generator-Desktop-$MODE/libSMB1_Compliance_Generator.so $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Generators/SMB1_Compliance_Generator.so
-cp -rf $SOURCE_LOCATION/Level-Headed/SMB1/build-SMB1_Compliance_To_SMB1-Desktop-$MODE/libSMB1_Compliance_To_SMB1.so $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
-cp -rf $SOURCE_LOCATION/Level-Headed/SMB1/build-SMB1_Writer-Desktop-$MODE/libSMB1_Writer.so $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Writers/SMB1_Writer.so
-cp -rf $SOURCE_LOCATION/Hexagon/build-Hexagon-Desktop-$MODE/libHexagon.so $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Hexagon.so
-cp -rf $SOURCE_LOCATION/Sequential_Archive/build-Sequential_Archive-Desktop-$MODE/libSequential_Archive.so $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Plugins/Sequential_Archive.so
+mkdir -p "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Data/SMB1/
+mkdir -p "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Generators/
+mkdir -p "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Interpreters/
+mkdir -p "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Writers/
+cp -rf "$SOURCE_LOCATION"/Level-Headed/SMB1/build-SMB1_Compliance_Generator-Desktop-"$MODE"/libSMB1_Compliance_Generator.so "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Generators/SMB1_Compliance_Generator.so
+cp -rf "$SOURCE_LOCATION"/Level-Headed/SMB1/build-SMB1_Compliance_To_SMB1-Desktop-"$MODE"/libSMB1_Compliance_To_SMB1.so "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Interpreters/SMB1_Compliance_To_SMB1.so
+cp -rf "$SOURCE_LOCATION"/Level-Headed/SMB1/build-SMB1_Writer-Desktop-"$MODE"/libSMB1_Writer.so "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Writers/SMB1_Writer.so
+cp -rf "$SOURCE_LOCATION"/Hexagon/build-Hexagon-Desktop-"$MODE"/libHexagon.so "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Hexagon.so
+cp -rf "$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive-Desktop-"$MODE"/libSequential_Archive.so "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Plugins/Sequential_Archive.so
 
 echo "Installing plugin for SAM..."
-mkdir -p $SOURCE_LOCATION/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-$MODE/Plugins/
-cp -rf $SOURCE_LOCATION/Sequential_Archive/build-Sequential_Archive-Desktop-$MODE/libSequential_Archive.so $SOURCE_LOCATION/build-Sequential_Archive_Manager-Desktop-$MODE/Plugins/Sequential_Archive.so
+mkdir -p "$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-"$MODE"/Plugins/
+cp -rf "$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive-Desktop-"$MODE"/libSequential_Archive.so "$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-"$MODE"/Plugins/Sequential_Archive.so
 
 echo "Packing Graphics and Music..."
-$SOURCE_LOCATION/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-$MODE/Sequential_Archive_Manager --pack $SOURCE_LOCATION/Level-Headed_Data/Graphics $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Data/SMB1/Graphics.sa
-$SOURCE_LOCATION/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-$MODE/Sequential_Archive_Manager --pack $SOURCE_LOCATION/Level-Headed_Data/Music $SOURCE_LOCATION/Level-Headed/build-Level-Headed-Desktop-$MODE/Data/SMB1/Music.sa
+"$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-"$MODE"/Sequential_Archive_Manager --pack "$SOURCE_LOCATION"/Level-Headed_Data/Graphics "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Data/SMB1/Graphics.sa
+"$SOURCE_LOCATION"/Sequential_Archive/build-Sequential_Archive_Manager-Desktop-"$MODE"/Sequential_Archive_Manager --pack "$SOURCE_LOCATION"/Level-Headed_Data/Music "$SOURCE_LOCATION"/Level-Headed/build-Level-Headed-Desktop-"$MODE"/Data/SMB1/Music.sa
 
 echo "Runtime Environment Prepared!"
 exit 0
