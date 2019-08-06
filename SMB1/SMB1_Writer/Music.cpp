8f0ec8215768c24fac30996a7c3a14b7637c8915
Coolcord <coolcord24@tutanota.com>
2019-08-06 00:33:37 -0700

    Disable Music now disables all music

--- SMB1/SMB1_Writer/Music.cpp
+++ SMB1/SMB1_Writer/Music.cpp
@@ -8,11 +8,7 @@ Music::Music(QFile *file, Level_Offset *levelOffset, Sequential_Archive_Handler 
 }
 
 bool Music::Disable_Music() {
-    if (!this->Write_Bytes_To_Offset(0x1124, QByteArray(1, static_cast<char>(0x49)))) return false; //disables most music
-    if (!this->Write_Bytes_To_Offset(0x582E, QByteArray(1, static_cast<char>(0x82)))) return false; //disables Starman music
-    if (!this->Write_Bytes_To_Offset(0x7920, QByteArray(1, static_cast<char>(0x4B)))) return false; //disables castle complete theme
-    if (!this->Write_Bytes_To_Offset(0x7922, QByteArray(1, static_cast<char>(0x4B)))) return false; //disables level complete theme
-    return true;
+    return this->Write_Bytes_To_Offset(0x7364, QByteArray::fromHex(QString("EAEAEA").toLatin1()));
 }
 
 bool Music::Tone_Color_1() {
