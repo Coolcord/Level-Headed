a6c41357cd84d81f5437f3058b0f911a45f61a10
Coolcord <coolcord24@tutanota.com>
2019-08-17 14:12:54 -0700

    The Git update checker can now check multiple URLs

--- Level-Headed/Update_Thread.cpp
+++ Level-Headed/Update_Thread.cpp
@@ -15,16 +15,22 @@ Update_Thread::Update_Thread(QWidget *parent, QApplication *application, Readabl
 
 void Update_Thread::run() {
     //UPDATE CHECKER SETTINGS
-    const QString REMOTE_GIT_SERVER = "https://github.com/Coolcord/Level-Headed.git";
-    const QString UPDATE_PAGE = "https://github.com/Coolcord/Level-Headed/releases";
+    const QString GITHUB_UPDATE_PAGE = "https://github.com/Coolcord/Level-Headed/releases";
+    const QString GITLAB_UPDATE_PAGE = "https://gitlab.com/Coolcord/Level-Headed";
+    QStringList REMOTE_GIT_SERVERS;
+    REMOTE_GIT_SERVERS << "https://github.com/Coolcord/Level-Headed.git";
+    REMOTE_GIT_SERVERS << "git@github.com:Coolcord/Level-Headed.git";
+    REMOTE_GIT_SERVERS << "https://gitlab.com/Coolcord/Level-Headed.git";
+    REMOTE_GIT_SERVERS << "git@gitlab.com:Coolcord/Level-Headed.git";
     const bool FORCE_UPDATE_CHECK = false;
 
     //Run the update check
-    QString lastIgnoredUpdate = QString();
+    QString lastIgnoredUpdate = QString(), foundViaUrl = QString(), updatePage = GITHUB_UPDATE_PAGE;
     Git_Update_Checker gitUpdateChecker;
     if (this->readableConfigFile->Get_Value("Last_Ignored_Update", lastIgnoredUpdate) &&
             gitUpdateChecker.Is_Version_Newer_Than_Current(lastIgnoredUpdate, this->version)) this->version = lastIgnoredUpdate;
     if (FORCE_UPDATE_CHECK) this->version = "0.0.0";
-    QString newVersion = gitUpdateChecker.Check_For_Updates(this->version, REMOTE_GIT_SERVER, this->gitLocation);
-    if (!newVersion.isEmpty()) emit Update_Available(newVersion, UPDATE_PAGE);
+    QString newVersion = gitUpdateChecker.Check_For_Updates(this->version, foundViaUrl, REMOTE_GIT_SERVERS, this->gitLocation);
+    if (foundViaUrl.contains("gitlab")) updatePage = GITLAB_UPDATE_PAGE;
+    if (!newVersion.isEmpty()) emit Update_Available(newVersion, updatePage);
 }
