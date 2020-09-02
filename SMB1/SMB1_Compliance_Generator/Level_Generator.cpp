4de960eebbef06d5859cd0defa81a2f1ad8c2e3b
Coolcord <coolcord24@tutanota.com>
2020-09-01 20:35:14 -0700

    Bullet Bill Cannons are now removed 15 units after a new page instead of 16

--- SMB1/SMB1_Compliance_Generator/Level_Generator.cpp
+++ SMB1/SMB1_Compliance_Generator/Level_Generator.cpp
@@ -95,7 +95,7 @@ void Level_Generator::Remove_Unsafe_Cannons_From_Level() {
     assert(this->objects);
     if (this->args->levelCompliment != Level_Compliment::BULLET_BILL_TURRETS) return; //no cannons can exist on the level
     int midpointPageX = this->midpointHandler->Get_Midpoint()*16;
-    int minInvalidMidpointX = midpointPageX, maxInvalidMidpointX = midpointPageX+16;
+    int minInvalidMidpointX = midpointPageX, maxInvalidMidpointX = midpointPageX+15;
 
     this->objects->Seek_To_First_Item();
     while (!this->objects->At_End()) {
@@ -105,7 +105,7 @@ void Level_Generator::Remove_Unsafe_Cannons_From_Level() {
             break;
         case Object_Item::BULLET_BILL_TURRET:
             if (data->y == 0x9 || data->y == 0xA) { //check if the cannon's height is in range of Mario standing on the ground
-                if (data->absoluteX <= 16) { //remove the cannon if it is on the first page of the level
+                if (data->absoluteX < 16) { //remove the cannon if it is on the first page of the level
                     data->objectItem = Object_Item::VERTICAL_BLOCKS;
                 } else { //remove the cannon if it is on the midpoint page
                     if (data->absoluteX >= minInvalidMidpointX && data->absoluteX <= maxInvalidMidpointX) data->objectItem = Object_Item::VERTICAL_BLOCKS;
