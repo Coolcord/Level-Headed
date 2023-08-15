f0d54ffc3510dbba62fb24f09484c08b7c6d42f1
Coolcord <coolcord24@tutanota.com>
2023-08-15 15:47:41 -0700

    Installer script now makes use of autogenerated sections

--- Level-Headed/Build_Scripts/Unix/Deploy_New_Version.sh
+++ Level-Headed/Build_Scripts/Unix/Deploy_New_Version.sh
@@ -60,12 +60,85 @@ mv ./Level-Headed ./Deployed_Files/
 cd ./Deployed_Files
 
 # Zip up Level-Headed archive for users who don't want an installer
+echo ""; echo "Creating 7zip archive..."
 mv ./Level-Headed/ "./Level-Headed $version"
 7z a "./Level-Headed $version.7z" "./Level-Headed $version"
 mv "./Level-Headed $version" ./Level-Headed/
 
+# Prepare Installer for Compilation
+echo ""; echo "Compiling Level-Headed Installer..."
+startRmdirSectionMarker="# --------------- BEGIN AUTO-GENERATED RMDIR SECTION --------------- #"
+endRmdirSectionMarker="# ---------------- END AUTO-GENERATED RMDIR SECTION ---------------- #"
+startSetOutPathSectionMarker="# ------------- BEGIN AUTO-GENERATED SETOUTPATH SECTION ------------ #"
+endSetOutPathSectionMarker="# -------------- END AUTO-GENERATED SETOUTPATH SECTION ------------- #"
+startFileSectionMarker="# --------------- BEGIN AUTO-GENERATED FILE SECTION ---------------- #"
+endFileSectionMarker="# ---------------- END AUTO-GENERATED FILE SECTION ----------------- #"
+installerFile="$localSourceCodeLocation"/Level-Headed/Level-Headed/Build_Scripts/Windows/Installer/Level-Headed.nsi
+rm rmdirSection.txt setOutPathSection.txt fileSection.txt > /dev/null 2>&1
+
+# Prepare RMDIR Section
+find ./Level-Headed -maxdepth 1 -type d -print > directories.txt
+sed -i 's|/|\\|g' directories.txt
+sed -i 's|\.\\Level-Headed|\$INSTDIR|g' directories.txt
+sed -i '/\\Config/d' directories.txt # don't delete config directories
+while IFS= read -r directory; do
+    echo RmDir /r \""$directory"\" >> rmdirSection.txt
+done < directories.txt
+sed -i '/\"$INSTDIR\"/d' directories.txt # don't delete the base directory
+sed -i '/^RmDir \/r "$INSTDIR"$/d' rmdirSection.txt
+
+# Prepare SetOutputPath Section
+find ./Level-Headed -type d -print > directories.txt
+sed -i 's|/|\\|g' directories.txt
+sed -i 's|\.\\Level-Headed|\$INSTDIR|g' directories.txt
+while IFS= read -r directory; do
+    echo SetOutPath \""$directory"\" >> setOutPathSection.txt
+done < directories.txt
+
+# Prepare File Section
+find ./Level-Headed -type f -print > files.txt
+sed -i "s|\.\/|$localSourceCodeLocation\/|g" files.txt
+while IFS= read -r file; do
+    echo File \""$file"\" >> fileSection.txt
+done < files.txt
+
+convert_unix_paths_to_windows() {
+    sed -i 's|/a/|A:/|g; s|/b/|B:/|g; s|/c/|C:/|g; s|/d/|D:/|g; s|/e/|E:/|g; s|/f/|F:/|g; s|/g/|G:/|g; s|/h/|H:/|g; s|/i/|I:/|g; s|/j/|J:/|g; s|/k/|K:/|g; s|/l/|L:/|g; s|/m/|M:/|g; s|/n/|N:/|g; s|/o/|O:/|g; s|/p/|P:/|g; s|/q/|Q:/|g; s|/r/|R:/|g; s|/s/|S:/|g; s|/t/|T:/|g; s|/u/|U:/|g; s|/v/|V:/|g; s|/w/|W:/|g; s|/x/|X:/|g; s|/y/|Y:/|g; s|/z/|Z:/|g; s|/|\\|g' "$1"
+}
+
+convert_unix_paths_to_windows "setOutPathSection.txt"
+convert_unix_paths_to_windows "fileSection.txt"
+
+update_section() {
+    # Find the line numbers of the markers
+    startLine=$(grep -n "$1" "$installerFile" | cut -d ':' -f 1)
+    endLine=$(grep -n "$2" "$installerFile" | cut -d ':' -f 1)
+
+    # Check if markers were found
+    if [ -z "$startLine" ] || [ -z "$endLine" ]; then
+      echo "Markers not found in $installerFile"
+      exit 1
+    fi
+
+    # Calculate the line range to delete
+    deleteRange="$((startLine+1)),${endLine}d"
+    
+    # Restore the end section marker
+    echo $2 >> $3
+
+    # Delete lines between markers and insert contents of fileSection.txt
+    sed -i "$deleteRange" "$installerFile"
+    sed -i "${startLine}r $3" "$installerFile"
+}
+
+update_section "$startRmdirSectionMarker" "$endRmdirSectionMarker" "rmdirSection.txt"
+update_section "$startSetOutPathSectionMarker" "$endSetOutPathSectionMarker" "setOutPathSection.txt"
+update_section "$startFileSectionMarker" "$endFileSectionMarker" "fileSection.txt"
+rm rmdirSection.txt setOutPathSection.txt fileSection.txt directories.txt files.txt
+
 # Compile the Level-Headed installer for users who want an installer
 # TODO: Write this...
+echo "Done!"
 
 echo ""; echo "Deploy Complete! Enjoy Level-Headed!"
 exit 0
